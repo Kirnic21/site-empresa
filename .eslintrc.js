9ddb0b9f6d64b7b51596ac8b081f877c125445ea
MarcecloK <marcelokenji12121@gmail.com>
2023-07-20 15:19:09 -0300

    Initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es2021": true
+    },
+    "extends": "eslint:recommended",
+    "overrides": [
+    ],
+    "parserOptions": {
+        "ecmaVersion": "latest",
+        "sourceType": "module"
+    },
+    "rules": 
+      { 
+        "extends": [
+          "some-other-config-you-use",
+          "prettier"
+        ]
+      }
+}
